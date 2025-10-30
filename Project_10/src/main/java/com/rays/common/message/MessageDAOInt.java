5497f62a60e9c382b43329b59253455e8fcb002a
sushobhitp11 <sushobhitp11@gmail.com>
2025-10-30 16:47:22 +0530

    30-10-2025

---
+++ Project_10/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Sushobhit Pandey 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
